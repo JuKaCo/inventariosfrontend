7680abc752dd2c07c0229ecce868172c63ba690f
rnavia <roger.navia.g@gmail.com>
2021-03-18 10:46:08 -0400

    ultimo load y servicio implementado

--- src/app/general/login/login.component.ts
+++ src/app/general/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoaderService } from '../services/loader.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
  
-  constructor() { }
+  constructor(
+    private loaderService: LoaderService,
+  ) { }
 
   ngOnInit(): void {
   }
-
+  ingresar(){
+    console.log('--->ingresar');
+    this.loaderService.show();
+  }
 }

351514e9366ed0488f1b5ad5d25a839c54e30b04
rnavia <roger.navia.g@gmail.com>
2021-04-01 15:28:41 -0400

    modif verif

--- src/app/general/config-auth-config/auth.guard.ts
+++ src/app/general/config-auth-config/auth.guard.ts
@@ -7,32 +7,36 @@ import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, private oauthService: OAuthService) {}
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Promise<boolean> {
+  constructor(private router: Router, private oauthService: OAuthService) { }
+  /*canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Promise<boolean> {
+    if (this.oauthService.hasValidIdToken()) {
+      return true;
+    }
 
+    const promise = new Promise<boolean>((resolve, reject) => {
+      return this.oauthService.loadDiscoveryDocumentAndTryLogin().then((success: boolean) => {
+        if (!success) {
+          this.router.navigate(['/']);
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+    return promise;
+  }*/
+
+  canActivate(): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    console.log('---->',this.oauthService.hasValidAccessToken());
     if (
-      this.oauthService.hasValidAccessToken() 
-      &&  this.oauthService.hasValidIdToken()
+      this.oauthService.hasValidAccessToken()
     ) {
-      console.log('2--->');
       return true;
     } else {
       this.router.navigate(['/']);
       return false;
     }
   }
-
-  /*canActivate(): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    if (
-      this.oauthService.hasValidAccessToken() &&
-      this.oauthService.hasValidIdToken()
-    ) {
-      return true;
-    } else {
-      this.router.navigate(['/']);
-      return false;
-    }
-  }*/
 }

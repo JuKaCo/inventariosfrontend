7ab1b0a223a6dfeff5dc468240c09a740d50dc6f
rnavia <roger.navia.g@gmail.com>
2021-04-20 18:07:11 -0400

    sesson url menu element

--- src/app/general/breadcrumb-general/breadcrumb-general.component.ts
+++ src/app/general/breadcrumb-general/breadcrumb-general.component.ts
@@ -10,12 +10,12 @@ import { MenuItem } from 'primeng/api';
 export class BreadcrumbGeneralComponent implements OnInit {
   items: MenuItem[] = [];
   home!: MenuItem;
-  constructor(private router: Router) { 
+  constructor(private router: Router) {
 
   }
 
   ngOnInit(): void {
-   this.cargar()
+    this.cargar()
     //let res = this.router.url.split("/");
     /* res.forEach(element => {
        if (element != '') {
@@ -67,17 +67,24 @@ export class BreadcrumbGeneralComponent implements OnInit {
     return { "nivel1": "", "nivel2": "" }
   }
 
-   cargar(){
+  cargar() {
     let menu: any = sessionStorage.getItem('menu_gen');
     if (menu) {
       menu = JSON.parse(menu);
-      let bc = this.getNameMenu(menu, this.router.url);
-      if (bc.nivel1 != "") {
-        this.items.push({ label: bc.nivel1.label });
+      let menuSelected = this.getNameMenu(menu, this.router.url);
+      if (menuSelected.nivel1 != "") {
+        this.items.push({ label: menuSelected.nivel1.label });
       }
-      if (bc.nivel2 != "") {
-        this.items.push({ label: bc.nivel2.label });
+      if (menuSelected.nivel2 != "") {
+        this.items.push({ label: menuSelected.nivel2.label });
       }
+      if (menuSelected.nivel2 != "") {
+        sessionStorage.setItem("menu_gen_active", JSON.stringify(menuSelected.nivel2.permisos))
+      } else {
+        sessionStorage.setItem("menu_gen_active", JSON.stringify(menuSelected.nivel1.permisos))
+      }
+      if (menuSelected.nivel1 == "")
+        sessionStorage.removeItem("menu_gen_active");
     }
     this.home = { icon: 'pi pi-home', routerLink: '/incio' };
   }
